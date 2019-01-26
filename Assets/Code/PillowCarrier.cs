5103eb9347b5ed1c9ba7c5605d84e90a1e598bf2
Rory Dungan <rorydungan@gmail.com>
2019-01-26 12:24:14 +1000

    Rotate player to face movement direction

---
+++ Assets/Code/PillowCarrier.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using UnityEngine;
+using Zenject;
+
+namespace ElMoro
+{
+    /// <summary>
+    /// Object that can pick up pillows.
+    /// </summary>
+    public class PillowCarrier : MonoBehaviour
+    {
+        [SerializeField]
+        private int playerIndex = 0;
+
+        [SerializeField]
+        private float pickupDistance = 1f;
+
+        [Inject]
+        private IInputManager InputManager { get; set; }
+
+        private const string PillowTag = "Pillow";
+
+        private Transform currentPillow = null;
+
+        private void Update()
+        {
+            if (InputManager.GetGrabButtonDown(playerIndex))
+            {
+                var newPillow = AttemptGrab();
+                if (newPillow != null)
+                {
+                    newPillow.transform.SetParent(transform);
+                    currentPillow = newPillow;
+                }
+            }
+
+            if (!InputManager.GetGrabButton(playerIndex) && currentPillow != null)
+            {
+                currentPillow.SetParent(null);
+            }
+        }
+
+        /// <summary>
+        /// Attempt to grab for a pillow. Returns the grabbed pillow, or null
+        /// if none was found.
+        /// </summary>
+        private Transform AttemptGrab()
+        {
+            // TODO: play grab animation
+
+            var hits = Physics.RaycastAll(
+                new Ray(transform.position, transform.forward),
+                pickupDistance
+            );
+
+            return hits.Select(h => h.transform)
+                .Where(t => t.CompareTag(PillowTag))
+                .OrderBy(t => (t.position - transform.position).sqrMagnitude)
+                .FirstOrDefault();
+        }
+    }
+}
