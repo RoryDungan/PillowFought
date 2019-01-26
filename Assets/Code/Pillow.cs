420afc917049405b11d280b2395c9d117bc8429f
Rory Dungan <rorydungan@gmail.com>
2019-01-26 13:17:32 +1000

    Start work on pillow script

---
+++ Assets/Code/Pillow.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+namespace ElMoro
+{
+    /// <summary>
+    /// A throwable pillow.
+    /// </summary>
+    public interface IPillow
+    {
+        /// <summary>
+        /// Pick up the pillow and attach it to the specified object. The
+        /// pillow must not be being carried.
+        /// </summary>
+        void Grab(Transform newParent);
+
+        /// <summary>
+        /// Drop the pillow. The pillow must be being carried first.
+        /// </summary>
+        void Drop();
+
+        /// <summary>
+        /// Throw the pillow in the specified direction. The pillow must be
+        /// being carried.
+        /// </summary>
+        void Throw(Vector3 direction);
+    }
+
+    public class Pillow : MonoBehaviour, IPillow
+    {
+        private new Rigidbody rigidbody;
+
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            if (rigidbody == null)
+            {
+                throw new Exception("Could not find Rigidbody component on Pillow.");
+            }
+        }
+
+        public void Drop()
+        {
+            transform.SetParent(null);
+            rigidbody.isKinematic = false;
+        }
+
+        public void Grab(Transform newParent)
+        {
+            transform.SetParent(newParent);
+            rigidbody.isKinematic = true;
+        }
+
+        public void Throw(Vector3 direction)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
