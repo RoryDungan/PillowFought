79c0e3d80b5d7c0226023fe06133745b8363eb8d
Jacob Duniam <duniamjj@gmail.com>
2019-01-26 18:04:14 +1000

    - Organised my files  - Added logic for moving from the initial scene into the proper game scene

---
+++ Assets/Code/Window Management/Scripts/SceneCoordinator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCoordinator : MonoBehaviour
+{
+    public bool displayStartWindowOnStart;
+    public WindowSchematic startWindow;
+    public WindowSchematic finishWindow;
+    public WindowSchematic trackingWindow;
+
+    private GameObject startWindowInstance;
+    private GameObject finishWindowInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (displayStartWindowOnStart)
+        {
+            startWindowInstance = WindowCreator.instance.CreateWindow(startWindow);
+            startWindowInstance.GetComponentInChildren<StartMenuLogic>().Populate(StartGame, QuitGame);
+        }
+    }
+
+    /// <summary>
+    /// event for loading the right scene for starting the game
+    /// </summary>
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("gameScene");
+        SceneManager.UnloadSceneAsync("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
