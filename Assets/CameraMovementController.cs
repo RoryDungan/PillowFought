ab81388d529bb92b8ac99923ad5ca7e9d80b662b
Jacob Duniam <duniamjj@gmail.com>
2019-01-26 23:04:35 +1000

    - add camera shake to a camera prefab

---
+++ Assets/CameraMovementController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour
+{
+    public static CameraMovementController instance;
+    private Animator anim;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            ShakeTheCamera(0.3f);
+        }
+    }
+
+    public async void ShakeTheCamera(float seconds)
+    {
+        anim.SetBool("shake", true);
+
+        await Task.Delay((int)(seconds * 1000));
+
+        anim.SetBool("shake", false);
+
+    }
+}
