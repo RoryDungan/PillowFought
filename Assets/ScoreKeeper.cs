09abed4e0ecabc239dbe93afa2f396c3e2d7ec43
Jacob Duniam <duniamjj@gmail.com>
2019-01-27 16:10:41 +1000

    - Add a scorekeeper asset

---
+++ Assets/ScoreKeeper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public Text player1Score;
+    public Text player2Score;
+
+    public static ScoreKeeper instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void UpdateScore(int whichPlayer, int score)
+    {
+        if (whichPlayer == 1)
+            player1Score.text = $"Player 1: {score}";
+        else
+            player2Score.text = $"Player 2: {score}";
+    }
+}
